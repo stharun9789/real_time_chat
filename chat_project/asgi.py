16918f6df0e48d70ea533a0f3c545925922a7b90
Tharun <144086666+stharun9789@users.noreply.github.com>
2025-03-16 14:49:53 +0530

    Fix WebSocket routing

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -2,6 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from chat.routing import websocket_urlpatterns  # Import WebSocket routes
 import chat.routing  # Make sure 'chat.routing' exists and is correct
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_project.settings")
